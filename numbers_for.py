1e0a427ff60dd1c6ab6bf135fd8da87d658c2c48
藤原未来子 <fujiwara_mikiko@fujiwaramiraikononotobukkukonpyuta.local>
2026-01-03 19:31:06 +0900

    update

--- numbers_for.py
+++ numbers_for.py
@@ -1,13 +1,15 @@
 numbers = [3, 7, 2, 9, 4]
 
-# 1. 全部出力
+# 1. 全部出力  #リストはリストの順に出る
 for number in numbers:
     print(number)
 
-# 2. 偶数だけ出力
+# 2. 偶数だけ出力　　
 for number in numbers:
     if number % 2 == 0:
         print(number)
 
 # 3. 最大値を出力
 print(max(numbers))
+
+print('done')
